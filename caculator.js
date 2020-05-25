935d8e90a6a842f05ec1b65d6f64d2ad29ffde13
Rea Sakyo <cuong6218@gmail.com>
2020-05-25 11:30:01 +0700

    update file tinhdientichhcn, tinh dien tich hinh chu nhat bang nhung file js

---
+++ caculator.js
@@ -0,0 +1,8 @@
+    let inputWidth;
+        let inputHeight;
+        inputWidth = prompt('Nhập chiều dài:',);
+        inputHeight = prompt('Nhập chiều rộng:',);
+        let width = parseInt(inputWidth);
+        let height = parseInt(inputHeight);
+        area = width*height;
+        document.write("the area is:"+area);
