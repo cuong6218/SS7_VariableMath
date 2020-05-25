efececf74fa69e41795e838701adc1ed7d07c338
Rea Sakyo <cuong6218@gmail.com>
2020-05-25 12:56:28 +0700

    add file tinh dien tich, chu vi hinh tron, add file doi do C thanh do F

---
+++ hinhtron/caculatorhinhtron.js
@@ -0,0 +1,10 @@
+function tinhdientich(){
+    let r = +prompt('Nhập bán kính hình tròn:',);
+    let s = r*r*3.14;
+    document.write('Diện tích hình tròn là:'+s);
+}
+function tinhchuvi(){
+    let r = +prompt('Nhập bán kính hình tròn:',);
+    let c = 2*r*3.14;
+    document.write('Chu vi hình tròn là:'+c);
+}
