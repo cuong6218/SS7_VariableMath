ebdc9c092ee8fb5bfa1d390f9a8128a717f89e57
Rea Sakyo <cuong6218@gmail.com>
2020-05-26 12:35:10 +0700

    add file lam may tinh nang cao

---
+++ DOM/search.js
@@ -0,0 +1,15 @@
+function SearchAdvance(){
+    let a = document.getElementById("show_search_advance");
+    let b = document.getElementById("search_advance");
+    let text = a.innerHTML();
+    if (text == "Tìm kiếm nâng cao"){
+        
+        a.innerHTML() = "Bỏ tìm kiếm nâng cao";
+       
+            
+        }
+            
+        
+        return false;
+    }
+    
