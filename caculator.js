056f06004b2b13c8a11856f5305ba3794d132a55
Rea Sakyo <cuong6218@gmail.com>
2020-05-26 16:42:09 +0700

    cap nhat folder

--- caculator.js
+++
@@ -1,8 +0,0 @@
-    let inputWidth;
-        let inputHeight;
-        inputWidth = prompt('Nhập chiều dài:',);
-        inputHeight = prompt('Nhập chiều rộng:',);
-        let width = parseInt(inputWidth);
-        let height = parseInt(inputHeight);
-        area = width*height;
-        document.write("the area is:"+area);
--- máy tính đơn giản/cacularExtend.js
+++ máy tính đơn giản/cacularExtend.js
@@ -1,12 +1,12 @@
 function show(a){
     document.getElementById("textbox").value += a;
 }
-function clearEach(){
-    document.getElementById("textbox").value = document.getElementById("textbox").value - a;
-}
 function equal(){
     document.getElementById("textbox").value = eval (document.getElementById("textbox").value);
 }
 function clearScreen(){
     document.getElementById("textbox").value = null;    
+}
+function clearEach(){
+    document.getElementById("textbox").value = document.getElementById("textbox").value - a;
 }
